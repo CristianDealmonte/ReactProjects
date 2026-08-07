e9ff8195a6d8a7cfc10a70895f91ca9a29adcb7e
Cristian Frias <cristian.frias@daltile.com.mx>
2026-08-07 16:03:30 -0600

    Guitars load finished

--- 01.Guitarla/src/App.jsx
+++ 01.Guitarla/src/App.jsx
@@ -7,31 +7,52 @@ function App() {
 
   // Estados para el carrito de compras
   const [data, setData] = useState([]);
+  const [cart, setCart] = useState([]);
 
+  // Simulando la carga de datos desde una API o base de datos
   useEffect(() => {
-    // Simulando la carga de datos desde una API o base de datos
     setData(db);
   }, []);
+
+  function addToCart(item) {
+
+    const itemExists = cart.findIndex(element => element.id === item.id);
+    if(itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity ++;
+      setCart(updatedCart);
+      return;
+    }
+
+    item.quantity = 1;
+    setCart(prevState => [...prevState, item]);
+  }
   
   return (
     <>
-      <Header />
+      <Header 
+        cart={cart}
+      />
       
 
-      <main class="container-xl mt-5">
-          <h2 class="text-center">Nuestra Colección</h2>
+      <main className="container-xl mt-5">
+          <h2 className="text-center">Nuestra Colección</h2>
 
-          <div class="row mt-5">
-              <Guitar />
-              <Guitar />
-              <Guitar />
+          <div className="row mt-5">
+            {data.map((guitar) => (
+              <Guitar 
+                key={guitar.id}
+                guitar={guitar} 
+                addToCart={addToCart}
+              />
+            ))}
           </div>
       </main>
 
 
-      <footer class="bg-dark mt-5 py-5">
-          <div class="container-xl">
-              <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+      <footer className="bg-dark mt-5 py-5">
+          <div className="container-xl">
+              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
           </div>
       </footer>
 
--- 01.Guitarla/src/components/Guitar.jsx
+++ 01.Guitarla/src/components/Guitar.jsx
@@ -1,18 +1,22 @@
 
-export default function Guitar() {
+export default function Guitar({guitar, addToCart}) {
+
+    const { id, name, image, description, price } = guitar;
+
     
     return(
-        <div class="col-md-6 col-lg-4 my-4 row align-items-center">
-            <div class="col-4">
-                <img class="img-fluid" src="./public/img/guitarra_01.jpg" alt="imagen guitarra" />
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+            <div className="col-4">
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt={`imagen ${name}`} />
             </div>
-            <div class="col-8">
-                <h3 class="text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                <p class="fw-black text-primary fs-3">$299</p>
+            <div className="col-8">
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                <p>{description}</p>
+                <p className="fw-black text-primary fs-3">${price.toFixed(2)}</p>
                 <button 
                     type="button"
-                    class="btn btn-dark w-100"
+                    className="btn btn-dark w-100"
+                    onClick={ () => addToCart(guitar)}
                 >Agregar al Carrito</button>
             </div>
         </div>
