aed746b9e88f930f66ec847d65af92fcf17abea5
Cristian Dealmonte <cristian.04uri@gmail.com>
2026-08-11 23:38:27 -0600

    migrating GuitarLA to Typescript

---
+++ 01.Guitarla/src/components/Guitar.tsx
@@ -0,0 +1,31 @@
+interface Guitar {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+}
+
+export default function Guitar({guitar, addToCart} : {guitar : Guitar, addToCart : (guitar: Guitar) => void}) {
+
+    const { id, name, image, description, price } = guitar;
+
+    
+    return(
+        <div className="col-md-6 col-lg-4 my-4 row align-items-center">
+            <div className="col-4">
+                <img className="img-fluid" src={`/img/${image}.jpg`} alt={`imagen ${name}`} />
+            </div>
+            <div className="col-8">
+                <h3 className="text-black fs-4 fw-bold text-uppercase">{name}</h3>
+                <p>{description}</p>
+                <p className="fw-black text-primary fs-3">${price.toFixed(2)}</p>
+                <button 
+                    type="button"
+                    className="btn btn-dark w-100"
+                    onClick={ () => addToCart(guitar)}
+                >Agregar al Carrito</button>
+            </div>
+        </div>
+    )
+}
